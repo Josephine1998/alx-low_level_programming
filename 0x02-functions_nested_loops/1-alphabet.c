bed087efd52368eb1ff980d068fbe1581407c716
Josephine <josephinealiu98@gmail.com>
2023-04-26 02:18:00 -0700

    Islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_alphabet - print all alphabet in lowercase
  */
@@ -6,6 +7,6 @@ void print_alphabet(void)
 {
 	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		putchar(letter);
+	putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 void print_alphabet_x10(void)
 {
